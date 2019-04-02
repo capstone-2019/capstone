1aa441dd3ff04c0e51895842b68ffb60d447c85e
Joseph Kim <youngchk@andrew.cmu.edu>
2019-04-02 17:54:13 -0400

    implement audio_manager

--- backend/inc/audio_processor/file_output.hpp
+++ backend/inc/audio_processor/file_output.hpp
@@ -1,5 +1,5 @@
 /**
- * @file fileInput.hpp
+ * @file file_output.hpp
  * 
  * @brief conatins the API for FileInput, which parses and manages
  * audio files.
@@ -8,22 +8,20 @@
  * 
  */
 
-#ifndef _FILEINPUT_H_
-#define _FILEINPUT_H_
+#ifndef _FILEOUTPUT_H_
+#define _FILEOUTPUT_H_
 
 #include <vector>
-#include <input_interface.hpp>
-#include <audio_manager.hpp>
 
 /**
  * @brief Class for file inputs.
  *
  */
-class FileInput : public InputInterface
+class FileOutput
 {
 public:
 	/* cosntructor that takes in a filename and processes it */
-	FileInput(const char *filename, AudioManager::filetype_t file_type);
+	FileOutput(const char *filename, int samplerate);
 
 	/** @brief Defines iterator type over lines in the netlist */
     typedef std::vector<float>::iterator iterator;
@@ -42,15 +40,13 @@ public:
     /** @brief returns the number of frames */
 	int get_num_frames(){ return num_frames; };
 	/** @brief returns the samplerate */
-	int get_samplerate() override { return samplerate; };
+	int get_samplerate(){ return samplerate; };
 
 	/** @brief returns the next voltage */
-	bool get_next_value(float *val) override;
-	/** @brief resets the current index back to zero */
-	void reset_index() { cur_index = 0; }
+	void set_next_value(float val);
 
 	/** @brief saves the file as a cso file */
-	void save(const char* filename);
+	void finish();
 
 	/** @brief define subscript operator */
 	float &operator[](int i) {
@@ -58,14 +54,14 @@ public:
 	}
 
 private:
+	/** @brief name of the file to create */
+	const char *filename;
 	/** @brief vector containing frame data */
 	std::vector<float> frames;
 	/** @brief samplerate of the file */
 	int samplerate;
 	/** @brief number of frames the file contains */
 	int num_frames;
-	/** @brief index get_next_value will serve */
-	int cur_index;
 };
 
 #endif /* _FILEINPUT_H_ */
