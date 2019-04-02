6fc097ce909f7fd0c57067108695317cda737aa6
mjkasper <mkasper@andrew.cmu.edu>
2019-04-02 16:09:01 -0400

    Update audio manager interface

--- backend/inc/audio_processor/audio_manager.hpp
+++ backend/inc/audio_processor/audio_manager.hpp
@@ -1,9 +1,9 @@
 /**
  * @file audio_manager.hpp
- * 
+ *
  * @brief contains the API for the audio manager class, that is responsible for
  * managing audio input and output.
- * 
+ *
  * @author Joseph Kim (younghck)
  */
 
@@ -19,7 +19,11 @@ public:
 		INPUT_HARDWARE,
 	} input_t;
 
-	
+	typedef enum {
+		FILETYPE_WAV,
+		FILETYPE_TXT,
+	} filetype_t;
+
 	static constexpr const int OUTPUT_FILE = 1;
 	static constexpr const int OUTPUT_HARDWARE = (1 << 1);
 	typedef int output_t;
@@ -29,9 +33,11 @@ public:
 	/** @brief Constructor for one input or output file */
 	AudioManager(input_t input_mode, output_t output_mode,
 				 const char *filename) {}
+
 	/** @brief Constructor for both input and output files */
-	AudioManager(input_t input_mode, output_t output_mode, 
-				 const char *input_filename, const char *output_filename) {}
+	AudioManager(input_t input_mode, output_t output_mode,
+				 const char *input_filename, const char *output_filename,
+				 filetype_t infile_type) {}
 
 	/** @brief gets the next available value and stores it in val. Returns
 	    false when no more data is available. */
